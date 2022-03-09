d9bc5ee1a9429c27e33a8d1e1b4371e3e5cb312a
anchi <anchitsingha13579@gmail.com>
2022-03-09 18:27:08 +0530

    1st Push...

---
+++ app/src/main/java/com/example/github1stpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.github1stpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
